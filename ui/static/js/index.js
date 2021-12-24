8fb045f0cf85fb63d189999ba39517f8f67b650f
shuple <shuple@users.noreply.github.com>
2021-12-24 21:11:02 +0900

    modularize meta path

--- ui/static/js/index.js
+++ ui/static/js/index.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function () {
   const littleMagic = new LittleMagicMake();
   const init = async function() {
     await littleMagic.rest('/post/read',
-      { 'file': [ 'meta/make', 'meta/make_position' ], 'graphic': 'sfc', 'returnData': {} }, setMeta);
+      { 'file': [ 'meta/make/sprite', 'meta/make/position' ], 'graphic': 'sfc', 'returnData': {} }, setMeta);
     await littleMagic.rest('/post/read',
       { 'file': [ 'menu/make', 'stage/default' ], 'graphic': 'sfc', 'returnData': [] }, setSprite);
     littleMagic.makeContext();
