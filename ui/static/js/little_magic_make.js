cae953159d1862c29acaeb504f41e93879b28587
shuple <shuple@users.noreply.github.com>
2022-01-04 19:01:31 +0900

    improve readability

--- ui/static/js/little_magic_make.js
+++ ui/static/js/little_magic_make.js
@@ -2,13 +2,14 @@ import { LittleMagic } from './little_magic.js';
 export { LittleMagicMake }
 
 String.prototype.hashCode = function() {
-	let hash = 0, i, ch;
-	if (this.length === 0) return hash;
-	for (i = 0; i < this.length; i++) {
-		ch = this.charCodeAt(i);
-		hash  = ((hash << 5) - hash) + ch;
-		hash |= 0; // convert to 32bit integer
-	}
+  let hash = 0;
+  let i, ch;
+  if (this.length === 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    ch = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + ch;
+    hash |= 0; // convert to 32bit integer
+  }
 	return hash;
 };  // String.prototype.hashCode()
 
