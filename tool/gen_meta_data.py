461081d090dfd5106247012502b7a6363af4fdda
shuple <shuple@users.noreply.github.com>
2022-01-11 14:20:36 +0900

    generate meta/data.json

---
+++ tool/gen_meta_data.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import argparse, json, glob, os, sys
+path = f'{(os.path.dirname(os.path.abspath(__file__)))}/../data'
+
+def parse_args():
+    # options
+    parser = argparse.ArgumentParser(description='Generate meta sprite data')
+    parser.add_argument('-c', '--cg', type=int, default=0, help='CG type')
+    parser.add_argument('-f', '--file', default='data', help='save file name')
+
+    # convert to dict
+    return vars(parser.parse_args())
+#  def parse_args()
+
+# return return meta in dict
+#
+def meta_data(file, cg):
+    meta = {}
+    with open(f'{path}/system/{cg}/meta/{file}.json', 'r') as fp:
+        meta = json.loads(fp.read())
+        meta['lastBlock'] = block_count(cg)
+        meta['lastCG'] = cg_count()
+    return meta
+#  def meta_data()
+
+def block_count(cg):
+    file = max(glob.glob(f'{path}/image/sprite/{cg}/layer1/block/[0-9][0-9]'))
+    return int(os.path.basename(file))
+#  def block_count()
+
+def cg_count():
+    file = max(glob.glob(f'{path}/image/sprite/[0-9][0-9]'))
+    return int(os.path.basename(file))
+#  def cg_count():
+
+if __name__ == '__main__':
+    args = parse_args()
+    file = args['file']
+    cg = '%02i' % (args['cg'])
+    meta = meta_data(file, cg)
+    with open(f'{path}/system/{cg}/meta/{file}.json', 'w') as fp:
+        print(json.dumps(meta, indent=2))
+        fp.write(json.dumps(meta, indent=2))
+    #  with
+#  if
