267cde90cca55b0031997d9824a743a1846e2266
shuple <shuple@users.noreply.github.com>
2022-01-20 16:21:42 +0900

    generate all stages if stage is not given

--- tool/gen_stage_image.py
+++ tool/gen_stage_image.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import argparse, json, os, sys
+import argparse, glob, json, os, re, sys
 
 path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, f'{path}/../lib')
@@ -10,7 +10,7 @@ def parse_args():
     # options
     parser = argparse.ArgumentParser(description='Generate stage from json file')
     parser.add_argument('-c', '--cg', type=int, default=0, help='CG type')
-    parser.add_argument('file', nargs='?', type=int, default=1, help='stage file in json format')
+    parser.add_argument('file', nargs='?', type=int, default=0, help='stage file json format')
     parser.add_argument('-s', '--scale', type=float, default=2.0, help='scale of image')
 
     # convert to dict
@@ -21,8 +21,21 @@ if __name__ == '__main__':
     args = parse_args()
     o = lm.LittleMagicStage(args)
     o.load()
-    stage_file = '%s/../data/system/%02i/stage/%03i.json' % (path, args['cg'], args['file'])
-    with open(stage_file, 'r') as f:
-        data = json.load(f)
-        o.generate(args['file'], data)
+    stage_files = []
+    stage_path = '%s/../data/system/%02i/stage' % (path, args['cg'])
+    if args['file'] == 0:
+        for stage_file in sorted(glob.glob(f'{stage_path}/[0-9][0-9][0-9].json')):
+            stage_files.append(stage_file)
+    else:
+        stage_file = '%s/%03i.json' % (stage_path, args['file'])
+        stage_files.append(stage_file)
+    #  if
+    for stage_file in stage_files:
+        if os.path.exists(stage_file):
+            stage_num = int(re.search(r'(\d{3})\.json', stage_file)[1])
+            with open(stage_file, 'r') as f:
+                data = json.load(f)
+                o.generate(stage_num, data)
+            #  with
+    #  for
 #  if
