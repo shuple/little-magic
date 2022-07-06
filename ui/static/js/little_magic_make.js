373fd4b1276ae96ea710d5a23be7fa94bc9e27cb
shuple <shuple@users.noreply.github.com>
2022-07-07 05:40:26 +0900

    fix variable scope

--- ui/static/js/little_magic_make.js
+++ ui/static/js/little_magic_make.js
@@ -3,10 +3,9 @@ export { LittleMagicMake }
 
 String.prototype.hashCode = function() {
   let hash = 0;
-  let ch;
   if (this.length === 0) return hash;
   for (let i = 0; i < this.length; i++) {
-    ch = this.charCodeAt(i);
+    const ch = this.charCodeAt(i);
     hash  = ((hash << 5) - hash) + ch;
     hash |= 0; // convert to 32bit integer
   }
